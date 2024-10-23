52d226d9d00781984d132da161e713ab5512319d
juanert <61994784+juanert@users.noreply.github.com>
2024-10-23 10:03:01 -0400

    Update seccion1.js

--- Javascript/seccion1.js
+++ Javascript/seccion1.js
@@ -63,22 +63,25 @@ var mayor_o_igual = 5 >= 5;
 var menor_o_igual = 5 <= 8;
 
 //operadores logicos
+var y = true && false;
+var o = true || false;
+var negacion = !true;
 
 //Ejercicos para practicar la lógica
 // (Facil)
-var uno = !true || false;
-var dos = false && !false;
-var tres = true && !false;
+var uno = !true || false; //false
+var dos = false && !false; //false
+var tres = true && !false; //true
 
 //(Medio)
-var cuatro = (5 === 5) || (!true);
-var cinco = (!0) || (5 < 0);
-var seis = (3 > 4) && (!0);
+var cuatro = (5 === 5) || (!true); //true
+var cinco = (!0) || (5 < 0); //true
+var seis = (3 > 4) && (!0); //false
 
 //Avanzado
-var siete = ( ((false || true) && (!false && true) )) || (true && false)
-var ocho = (6 === 3+3) && (9/3 >= 3);
-var nueve = (!(!false) || ( ("1" == 1)) && (!false === true) ); 
+var siete = ((false || true) && (!false && true)) || (true && false) //true
+var ocho = (6 === 3+3) && (9/3 >= 3); //true
+var nueve = (!(!false) || ( ("1" == 1)) && (!false === true)); //true
 
 //Hardcore
 var diez = !(!true) === !( !( 5 >= 5 ) );
@@ -89,3 +92,36 @@ var doce = !( true && (!true === !( !(16 === (4*2) + 8))) ) !== (false);
 
 //Leyenda
 var trece = !(((!(!(10 / 5 == "2") === false) != false) === ((5 * 5) <= (100 / 4))) && (!((1 + 1) !== (10 / 5)) !== (true)));
+
+//Condicionales
+/*
+  Nos ayudan a tomar decisiones en nuestro código
+*/
+var edad = 18;
+if (edad < 0 || edad > 130){
+  console.log("Edad no valida");
+} else if(edad < 18){
+  console.log("Eres menor de edad");
+} else if(edad >= 18){
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Edad no valida");
+}
+
+/*Ejercicios para practicar la primera seccion*/
+/*
+  Crea tres variables, numero1, numero2 y operador y asignales un valor
+  Crea una condicion que verifique si el operador es +, -, *, /, % o **.
+  Dependiendo del operador, realiza la operacion correspondiente con los dos numeros
+  e imprime el resultado en la consola.
+
+  Crea una variable llamada dia y asignale un valor de 1 a 7
+  Crea una condicion que imprima el dia de la semana correspondiente al numero
+  que tenga la variable dia.
+
+  Nota: En ambos casos debes de evaluar que hacer en caso de que los valores
+  no sean validos.
+
+  Fecha de entrega: 22/10/2024
+*/
+
